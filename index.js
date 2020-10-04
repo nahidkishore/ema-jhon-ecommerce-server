b2924ddfe388ce2722608b0b03022f8bf12cc524
nahid <nahidkishore99@gmail.com>
2020-10-04 12:37:20 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 5000
+ const express = require('express')
+ const MongoClient = require('mongodb').MongoClient;
+ require('dotenv').config()
+    
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+   const app = express()
+   const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello ema watson!')
-})
+          
+  const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+       client.connect(err => {
+          const products = client.db("emaJhonStore").collection("products");
+         console.log('database connected');
+          });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+          app.listen(port)
