8e882f48a65bdadd620e749d855baf13fb2ba277
nahid <nahidkishore99@gmail.com>
2020-10-04 20:57:37 +0600

    load data from the server

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
  const express = require('express');
+ const app = express()
+ const port = 5000
  const bodyParser = require('body-parser');
  const cors = require('cors');
  const MongoClient = require('mongodb').MongoClient;
  require('dotenv').config()
     
- const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+ const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
    
- const app = express()
- app.use(bodyParser.json());
- app.use(cors());
 
- const port = 5000
-
- 
+app.use(cors());
+app.use(bodyParser.json());
       
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   
 client.connect(err => {
-   const productsCollection = client.db("emaJhonStore").collection("products");
+   const productsCollection = client.db("emaJhonEcommerce").collection("products");
        
-          app.post('/addProduct', (req,res) => {
+          app.post('/addProducts', (req,res) => {
             const products=req.body;
             console.log(products);
             productsCollection.insertMany(products)
             .then(result => {
               console.log(result.insertedCount);
-              res.send(result.insertedCount);
+              res.send(result.insertedCount)
             })
-          })
+      })
+          app.get('/products',(req,res) => {
+            productsCollection.find({})
+            .toArray((err,documents) =>{
+              res.send(documents);
+            })
+          }) 
+          //get load product single data
+          app.get('/product/:key',(req,res) => {
+            productsCollection.find({key: req.params.key})
+            .toArray((err,documents) =>{
+              res.send(documents[0]);
+            })
+          }) 
+
           });
 
 
-          app.listen(port)
+     app.listen(port);
