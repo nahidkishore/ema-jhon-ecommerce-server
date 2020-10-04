77c20f8706ad07fa6a3033ad5e66076a74710336
nahid <nahidkishore99@gmail.com>
2020-10-04 12:47:09 +0600

    connect database and create post endpoint use body-parsers and cors

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
- const express = require('express')
+ const express = require('express');
+ const bodyParser = require('body-parser');
+ const cors = require('cors');
  const MongoClient = require('mongodb').MongoClient;
  require('dotenv').config()
     
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-   const app = express()
-   const port = 5000
+   
+ const app = express()
+ const port = 5000
 
+ app.use(bodyParser.json());
+ app.use(cors());
           
   const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
        client.connect(err => {
           const products = client.db("emaJhonStore").collection("products");
-         console.log('database connected');
+       
+          app.post('/AddProducts',(req,res) => {
+            const product=req.body;
+            products.insertOne(product)
+            .then(result => {
+              console.log(result);
+            })
+          })
           });
 
 
